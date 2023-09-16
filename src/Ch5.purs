34719236899aef1c604f82a913e65a2d64225aae
zestsystem <mikeyim@unitetheculture.org>
2023-09-15 19:58:47 -0700

    learn: writing apply and flip apply

--- src/Ch5.purs
+++ src/Ch5.purs
@@ -1,9 +1,9 @@
 module Ch5 where
 
-import Prelude (Unit, show)
+import Prelude (Unit, show, discard )
 
 import Effect (Effect)
-import Effect.Console(log)
+import Effect.Console (log)
 
 flip :: ∀ a b c. (a -> b -> c) -> b -> a -> c
 flip f x y = f y x
@@ -11,7 +11,18 @@ flip f x y = f y x
 const :: ∀ a b. a -> b -> a
 const x _ = x
 
+apply :: ∀ a b. (a -> b) -> a -> b
+apply f x = f x
+
+infixr 0 apply as $
+
+applyFlipped :: ∀ a b. a -> (a -> b) -> b
+applyFlipped = flip apply
+
+infixl 1 applyFlipped as #
+
 test :: Effect Unit
 test = do
-    log (show (flip const 1 2 ))
+  log $ show $ flip const 1 2
+  flip const 1 2 # show # log
 
