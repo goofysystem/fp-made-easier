56c720c7f254c4d69704974e8667f487024b36c9
zestsystem <mikeyim@unitetheculture.org>
2023-09-21 22:14:04 -0700

    5.42. writing unzip

--- src/Ch5.purs
+++ src/Ch5.purs
@@ -160,6 +160,11 @@ zip _ Nil = Nil
 zip Nil _ = Nil
 zip (x : xs) (y : ys) = Tuple x y : zip xs ys
 
+unzip :: ∀ a b. List (Tuple a b) -> Tuple (List a) (List b)
+unzip Nil = Tuple Nil Nil
+unzip (Tuple x y : ts) =
+  unzip ts # \(Tuple xs ys) -> Tuple (x : xs) (y : ys)
+
 test :: Effect Unit
 test = do
   log $ show $ flip const 1 2
@@ -210,4 +215,7 @@ test = do
   log $ show $ zip (1 : 2 : 3 : Nil) ("a" : "b" : "c" : "d" : "e" : Nil)
   log $ show $ zip ("a" : "b" : "c" : "d" : "e" : Nil) (1 : 2 : 3 : Nil)
   log $ show $ zip (Nil :: List Unit) (1 : 2 : Nil)
+  log $ show $ unzip (Tuple 1 "a" : Tuple 2 "b" : Tuple 3 "c" : Nil)
+  log $ show $ unzip (Tuple "a" 1 : Tuple "b" 2 : Tuple "c" 3 : Nil)
+  log $ show $ unzip (Nil :: List (Tuple Unit Unit))
 
