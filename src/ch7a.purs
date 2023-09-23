d8a12801d3407e1e7a48fc16bae1ce4611c65b08
zestsystem <mikeyim@unitetheculture.org>
2023-09-23 15:40:33 -0700

    7.6. Writing Ord for Maybe

--- src/ch7a.purs
+++ src/ch7a.purs
@@ -1,8 +1,9 @@
 module Ch7a where
 
-import Prelude (Unit, show, discard, (==), ($))
+import Prelude (Unit, show, discard, (==), ($), (<), (>), (<=), (>=))
 
 import Data.Eq (class Eq)
+import Data.Ord (class Ord, Ordering(..), compare)
 import Effect (Effect)
 import Effect.Console (log)
 
@@ -13,6 +14,12 @@ instance eqMaybe :: Eq a => Eq (Maybe a) where
   eq (Just x) (Just y) = x == y
   eq _ _ = false
 
+instance ordMaybe :: Ord a => Ord (Maybe a) where
+  compare Nothing Nothing = EQ
+  compare (Just x) (Just y) = compare x y
+  compare Nothing _ = LT
+  compare _ Nothing = GT
+
 test :: Effect Unit
 test = do
   log $ show $ Just 5 == Just 5
@@ -20,4 +27,11 @@ test = do
   log $ show $ Just 5 == Nothing
   log $ show $ Nothing == Just 5
   log $ show $ Nothing == (Nothing :: Maybe Unit)
+  log "--------------------------"
+  log $ show $ Just 1 < Just 5
+  log $ show $ Just 5 <= Just 5
+  log $ show $ Just 5 > Just 10
+  log $ show $ Just 10 >= Just 10
+  log $ show $ Just 99 > Nothing
+  log $ show $ Just 99 < Nothing
 
