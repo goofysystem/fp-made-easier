a2acd01425da6144133439c9e97eea847f57f5b8
zestsystem <mikeyim@unitetheculture.org>
2023-09-22 20:13:25 -0700

    6.8. deriving instances using newtype

--- src/Ch6.purs
+++ src/Ch6.purs
@@ -1,5 +1,9 @@
 module Ch6 where
 
+import Data.Eq
+import Data.Newtype (class Newtype, unwrap)
+import Prelude (($))
+
 type Address =
   { street1 :: String
   , street2 :: String
@@ -8,7 +12,7 @@ type Address =
   , zip :: String
   }
 
-data Person = Person
+newtype Person = Person
   { name :: String
   , age :: Int
   , address :: Address
@@ -43,3 +47,25 @@ instance hasAddressResidence :: HasAddress Residence where
 instance hasAddressEmptyLot :: HasAddress EmptyLot where
   getAddress (EmptyLot l) = l.address
 
+newtype FirstName = FirstName String
+
+derive instance newTypeFirstName :: Newtype FirstName _
+derive instance eqFirstName :: Eq FirstName
+
+newtype Ceo = Ceo Person
+
+derive instance newtypeCeo :: Newtype Ceo _
+
+newtype Janitor = Janitor Person
+
+derive instance newtypeJanitor :: Newtype Janitor _
+
+genericPersonHasAddress :: ∀ a. Newtype a Person => a -> Address
+genericPersonHasAddress wrappedPerson =
+  getAddress $ unwrap wrappedPerson
+
+instance hasAddressCeo :: HasAddress Ceo where
+  getAddress = genericPersonHasAddress
+
+instance hasAddressJanitor :: HasAddress Janitor where
+  getAddress = genericPersonHasAddress
