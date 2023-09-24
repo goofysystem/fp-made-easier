29aa5f738ed77818449945e770ef58f497a91724
zestsystem <mikeyim@unitetheculture.org>
2023-09-23 19:03:13 -0700

    7.14. Deriving Eq, Ord, and Show for Maybe

--- src/ch7a.purs
+++ src/ch7a.purs
@@ -1,15 +1,17 @@
 module Ch7a where
 
-import Prelude (Unit, show, discard, (==), ($), (<), (>), (<=), (||), (<>))
+import Prelude (Unit, show, discard, (==), ($), (<), (>), (<=), (>=))
 
 import Data.Eq (class Eq)
-import Data.Ord (class Ord, Ordering(..), compare)
+import Data.Ord (class Ord)
 import Data.Show (class Show)
+import Data.Generic.Rep (class Generic)
+import Data.Show.Generic (genericShow)
 import Effect (Effect)
 import Effect.Console (log)
 
+{--
 data Maybe a = Nothing | Just a
-
 instance eqMaybe :: Eq a => Eq (Maybe a) where
   eq Nothing Nothing = true
   eq (Just x) (Just y) = x == y
@@ -31,6 +33,16 @@ infixl 4 greaterThanOrEq as >=
 instance showMaybe :: Show a => Show (Maybe a) where
   show Nothing = "Nothing"
   show (Just x) = "(Just " <> show x <> ")"
+--}
+
+data Maybe a = Nothing | Just a
+
+derive instance eqMaybe :: Eq a => Eq (Maybe a)
+derive instance ordMaybe :: Ord a => Ord (Maybe a)
+derive instance genericMaybe :: Generic (Maybe a) _
+
+instance showMaybe :: Show a => Show (Maybe a) where
+  show = genericShow
 
 test :: Effect Unit
 test = do
