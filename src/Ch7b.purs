d49fb39b236dbad58a4fae0ad614c892aeb7ed5c
zestsystem <mikeyim@unitetheculture.org>
2023-09-23 21:56:15 -0700

    7.25. Code for ToCSV

--- src/Ch7b.purs
+++ src/Ch7b.purs
@@ -2,6 +2,10 @@ module Ch7b where
 
 import Prelude
 
+import Data.Newtype (class Newtype)
+import Data.Generic.Rep (class Generic)
+import Data.Show.Generic (genericShow)
+
 import Effect (Effect)
 import Effect.Console (log)
 
@@ -9,6 +13,32 @@ newtype CSV = CSV String
 
 class ToCSV a where
   toCSV :: a -> CSV
+
+newtype FullName = FullName String
+
+instance showFullName :: Show FullName where
+  show (FullName name) = name
+
+newtype Age = Age Int
+
+derive instance newTypeAge :: Newtype Age _
+derive newtype instance showAge :: Show Age
+
+data Occupation = Doctor | Dentist | Lawyer | Unemployed
+
+derive instance genericOccupation :: Generic Occupation _
+
+instance showOccupation :: Show Occupation where
+  show = genericShow
+
+data Person = Person
+  { name :: FullName
+  , age :: Age
+  , occupation :: Occupation
+  }
+
+instance toCSVPerson :: ToCSV Person where
+  toCSV (Person { name, age, occupation }) = CSV $ show name <> "," <> show age <> "," <> show occupation
 
 test :: Effect Unit
 test = do
