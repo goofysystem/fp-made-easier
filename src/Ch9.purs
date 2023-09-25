ae787f359f225c5434052fe79623fd3fc53bbdc3
zestsystem <mikeyim@unitetheculture.org>
2023-09-24 22:47:03 -0700

    9.8. Code for Semigroup for AndBool

--- src/Ch9.purs
+++ src/Ch9.purs
@@ -1,9 +1,25 @@
 module Ch9 where
 
-import Prelude (Unit)
+import Prelude (Unit, class Show, class Eq, ($), discard, show)
 
 import Effect (Effect)
 import Effect.Console (log)
+
+import Data.Generic.Rep (class Generic)
+import Data.Show.Generic (genericShow)
+
+data AndBool = AFalse | ATrue
+
+derive instance eqAndBool :: Eq AndBool
+derive instance genericAndBool :: Generic AndBool _
+
+instance showAndBool :: Show AndBool where
+  show = genericShow
+
+instance semigroupAndBool :: Semigroup AndBool where
+  append ATrue ATrue = ATrue
+  append _ AFalse = AFalse
+  append AFalse _ = AFalse
 
 class Semigroup a where
   append :: a -> a -> a -- Combines 2 a's and produces an a. This meets the magma requirement of being cloesed under the binary operator <>.
@@ -15,4 +31,6 @@ class Semigroup a <= Monoid a where
 
 test :: Effect Unit
 test = do
-  log "placeholder"
+  log $ show $ ATrue <> ATrue
+  log $ show $ ATrue <> AFalse
+  log $ show $ AFalse <> AFalse
