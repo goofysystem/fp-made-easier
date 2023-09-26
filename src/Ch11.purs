ca8f6da483667ff9d24ad9972fb14fab6681acc6
zestsystem <mikeyim@unitetheculture.org>
2023-09-25 21:00:35 -0700

    11.7. Code for findMax

--- src/Ch11.purs
+++ src/Ch11.purs
@@ -13,8 +13,14 @@ max x y
   | x > y = x
   | otherwise = y
 
+findMax :: ∀ a. Ord a => a -> List a -> a
+findMax mx Nil = mx
+findMax mx (x : xs) = findMax (max x mx) xs
+
 test :: Effect Unit
 test = do
   log $ show $ reverse (10 : 20 : 30 : Nil)
   log $ show $ max (-1) 99
   log $ show $ max "aa" "z"
+  log $ show $ findMax 0 (37 : 311 : -1 : 2 : 84 : Nil)
+  log $ show $ findMax "" ("a" : "bbb" : "c" : Nil)
