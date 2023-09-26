813a3174124c3205d0cf553be9e0b485ad563ccc
zestsystem <mikeyim@unitetheculture.org>
2023-09-25 22:41:41 -0700

    11.17. Code for foldl1

--- src/Ch11.purs
+++ src/Ch11.purs
@@ -2,8 +2,9 @@ module Ch11 where
 
 import Data.List (List(..), (:), foldl)
 import Data.List.Types (NonEmptyList(..))
+import Data.Foldable (class Foldable)
 import Data.Maybe (Maybe(..))
-import Data.NonEmpty (NonEmpty(..), (:|))
+import Data.NonEmpty (NonEmpty, (:|))
 import Effect (Effect)
 import Effect.Console (log)
 import Prelude (class Ord, Unit, show, negate, discard, otherwise, type (~>), ($), (>))
@@ -21,7 +22,10 @@ findMax Nil = Nothing
 findMax l@(first : _) = Just $ foldl max first l
 
 findMaxNE :: ∀ a. Ord a => NonEmptyList a -> a
-findMaxNE (NonEmptyList (NonEmpty first l)) = foldl max first l
+findMaxNE (NonEmptyList ne) = foldl1 max ne
+
+foldl1 :: ∀ f a. Foldable f => (a -> a -> a) -> NonEmpty f a -> a
+foldl1 f (x :| xs) = foldl f x xs
 
 test :: Effect Unit
 test = do
