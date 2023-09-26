bf7f9c1cacd1fae6441fdca9016c5430c06ef200
zestsystem <mikeyim@unitetheculture.org>
2023-09-25 21:38:38 -0700

    11.11. Code for findMac using NonEmptyList

--- src/Ch11.purs
+++ src/Ch11.purs
@@ -1,7 +1,9 @@
 module Ch11 where
 
 import Data.List (List(..), (:), foldl)
+import Data.List.Types (NonEmptyList(..))
 import Data.Maybe (Maybe(..))
+import Data.NonEmpty (NonEmpty(..), (:|))
 import Effect (Effect)
 import Effect.Console (log)
 import Prelude (class Ord, Unit, show, negate, discard, otherwise, type (~>), ($), (>))
@@ -18,6 +20,9 @@ findMax :: ∀ a. Ord a => List a -> Maybe a
 findMax Nil = Nothing
 findMax l@(first : _) = Just $ foldl max first l
 
+findMaxNE :: ∀ a. Ord a => NonEmptyList a -> a
+findMaxNE (NonEmptyList (NonEmpty first l)) = foldl max first l
+
 test :: Effect Unit
 test = do
   log $ show $ reverse (10 : 20 : 30 : Nil)
@@ -25,3 +30,5 @@ test = do
   log $ show $ max "aa" "z"
   log $ show $ findMax (37 : 311 : -1 : 2 : 84 : Nil)
   log $ show $ findMax ("a" : "bbb" : "c" : Nil)
+  log $ show $ findMaxNE (NonEmptyList $ 37 :| (311 : -1 : 2 : 84 : Nil))
+  log $ show $ findMaxNE (NonEmptyList $ "a" :| ("bbb" : "c" : Nil))
