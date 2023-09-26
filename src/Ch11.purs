5bbe1474f4a3f35596626ae8edc324515530c3f6
zestsystem <mikeyim@unitetheculture.org>
2023-09-25 20:55:49 -0700

    11.5. Code for max

--- src/Ch11.purs
+++ src/Ch11.purs
@@ -3,11 +3,18 @@ module Ch11 where
 import Data.List (List(..), (:), foldl)
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude (Unit, show, type (~>), ($))
+import Prelude (class Ord, Unit, show, negate, discard, otherwise, type (~>), ($), (>))
 
 reverse :: List ~> List
 reverse = foldl (\rl x -> x : rl) Nil
 
+max :: ∀ a. Ord a => a -> a -> a
+max x y
+  | x > y = x
+  | otherwise = y
+
 test :: Effect Unit
 test = do
   log $ show $ reverse (10 : 20 : 30 : Nil)
+  log $ show $ max (-1) 99
+  log $ show $ max "aa" "z"
