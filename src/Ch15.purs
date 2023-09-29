f715cc6addf7669f61cad72418e1c17bd339b4fd
zestsystem <mikeyim@unitetheculture.org>
2023-09-29 16:37:44 -0700

    15.5. Code for runPredicate

--- src/Ch15.purs
+++ src/Ch15.purs
@@ -16,7 +16,15 @@ odd x = x .&. 0 == 0
 odd' :: Int -> Boolean
 odd' x = x `mod` 2 == 1
 
+data Predicate a = Predicate (a -> Boolean)
+
+runPredicate :: ∀ a. Predicate a -> a -> Boolean
+runPredicate (Predicate f) x = f x
+
 test :: Effect Unit
 test = do
   log $ show $ odd 0
   log $ show $ odd 1
+  log "------------------------"
+  log $ show $ runPredicate (Predicate odd) $ 10
+  log $ show $ runPredicate (Predicate odd) $ 11
