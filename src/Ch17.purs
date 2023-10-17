6433f76055353acd1e817fc05dbc46201754ccf4
zestsystem <mikeyim@unitetheculture.org>
2023-10-16 21:27:11 -0700

    17.2 Code for Applicative Instance for Maybe

--- src/Ch17.purs
+++ src/Ch17.purs
@@ -3,8 +3,30 @@ module Ch17 where
 import Prelude
 
 import Effect (Effect)
+import Data.Generic.Rep (class Generic)
+import Data.Show.Generic (genericShow)
 import Effect.Console (log)
+
+data Maybe a = Nothing | Just a
+
+derive instance genericMaybe :: Generic (Maybe a) _
+
+instance showMaybe :: Show a => Show (Maybe a) where
+  show = genericShow
+
+instance functorMaybe :: Functor Maybe where
+  map _ Nothing = Nothing
+  map f (Just x) = Just $ f x
+
+instance applyMaybe :: Apply Maybe where
+  apply (Just f) x = f <$> x
+  apply Nothing _ = Nothing
+
+instance applicativeMaybe :: Applicative Maybe where
+  pure = Just
 
 test :: Effect Unit
 test = do
-  log "placeholder"
+  log $ show $ (+) <$> Just 21 <*> Just 21
+  log $ show $ (*) <$> pure 2 <*> (pure 21 :: Maybe Int)
+  log $ show $ pure (+) <*> Just 17 <*> Just 25
